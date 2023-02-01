c13e3b4c9d66fededc99300272942ba0c27fab49
minasangG <111559035+minasangG@users.noreply.github.com>
2023-02-01 20:46:37 +0800

    Add files via upload

---
+++ DailyLog.java
@@ -0,0 +1,89 @@
+public class DailyLog {
+
+
+
+ January 13, 2023, Friday
+//-Discord call with Joshua
+//-Watched videos about Collections Framework
+//-Iterable, Comparators, Comparable, etc.
+//-30-minute code session/tutorial
+//-Exceptions
+//-Printing sysout and Exception w/ showDialog
+
+ January 14, 2023, Saturday
+//-Practiced Exceptions
+//-Finished Mosh topic about Collections
+//-ArrayList Practice
+
+ January 15, 2023, Sunday
+//-Completed Exception
+//-Constructors
+
+    January 16, 2023, Monday
+//-Discord call with Joshua
+//-Fixed Code Blockers on Exception practice project
+//-Spring Boot, SQL, Postman
+//-Rest API, GetMapping, PostMapping
+//-Create and Read on CRUD
+
+January 17, 2023, Tuesday
+//-Watched Daily Code Buffer and Maikke Bright Boost videos about Spring Boot
+//-Reviewed Baeldung’s Java guide on website about Loops and Exception
+
+    January 18, 2023, Wednesday
+//-Watched API 1 on Google Drive
+//-Discussion about Spring Boot flow
+//-API Review, Daily Code Buffer
+
+    January 19, 2023, Thursday
+//-Watched API 2 on Google Drive
+//-Discussed more on Spring Boot
+//-Coded Spring Boot samples
+
+
+
+    January 20, 2023, Friday
+//-Watched API 3
+//-Watched Daily Code Buffer about DSA
+//-DSA(Introduction, Stacks, Queues, Priority Queues, Linked Lists, Dynamic Array)
+
+    January 21, 2023, Saturday
+//-Watched api 3
+//            -Reviewed carapi, schoolapi, & coachapi
+//-Discord call about DSA
+//-DSA (LinkedList vs ArrayLists, Big O notation, Linear Search, Binary Search, Interpolation Search, Bubble Sort, Selection Sort, Insertion Sort)
+
+    January 22, 2023, Sunday
+//-Watched 3-hour video on SQL on Brocode
+//-Polished coachapi
+//-Re-Watched API 3
+
+    January 23-26, 2023, Monday-Thursday
+//-Waved around various videos that primarily talked about Spring Boot API framework
+//-2-hr Code Review of Lightsaver16’s API on clinicAppointment posted also on Github
+//-Finished patientRecordsAPI and started with coachAPI
+//-Exactly 5th month of coding with 1 month acted as a buffer after board licensure exams
+//-Learned how to add a repository, clone, and transfer files via Github Desktop through IntelliJ IDEA’s built-in system
+
+    January 27, 2023, Friday
+//-Spring Boot API Review
+//-Coded coachAPI and added controllers, models, repositories, and entities
+//-Read about mappers and DTOs
+
+    January 28, 2023, Saturday
+//-Attended 6hr Zuitt Free Bootcamp on Basic Web Development Workshop
+//-Workshop talked about HTML, CSS, Bootstrap, Git Bash, Github, and how stuff works inside
+//- 3-hr Pair coding session of LabV001 API primarily authored by Lightsaver16
+
+    January 29, 2023, Sunday
+//-Worked on coachAPI and re-installed code configurations on Models, Services, and Controllers
+
+    January 30, 2023, Monday
+-
+
+    January 31, 2023, Tuesday
+-
+
+    February 1, 2023
+
+}
